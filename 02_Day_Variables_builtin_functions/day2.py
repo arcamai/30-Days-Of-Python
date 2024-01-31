67a3f7a94d77739e27cf9b3a13796813354626fd
A R C A M A I <157503552+arcamai@users.noreply.github.com>
2024-01-31 17:19:23 +0100

    Create day2.py

---
+++ 02_Day_Variables_builtin_functions/day2.py
@@ -0,0 +1,19 @@
+# Day 2: 30 Days of python programming
+
+# LEVEL 1:
+first_name = 'Arcami'
+last_name = 'Eltz'
+full_name = 'Arcami Eltz'
+country = 'Germany'
+city = 'Berlin'
+age = 250
+year = 2014
+is_married = True
+is_true = True
+is_light = False
+
+first_name, last_name, country, age = 'Arcami', 'Eltz', 'Germany', 250
+
+# LEVEL 2:
+print(type(first_name))
+print(typle(age))
